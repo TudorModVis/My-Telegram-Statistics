4d4c9aa54aeeb502225ca337d5da4a7dc0a6782b
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-07-10 19:43:37 +0300

    title update

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 ## Overview
 
 This is the repository for a telegram statistics project that I made in order to play with ways to present data through charts, also seemed like a fun idea. The project is open-source, ensuring transparency. No personal information or data is being saved.
-**LIVE LINK : **  
+**LIVE LINK :** 
+
+## Data based on your chating hisotry
+
+- Messages timeline
+- Most messaged contacts
+- Most messages during the time of the day
+- Number of sent messages
+- Number of sent words
+- Most used words
+- Search how many times you used a word
+- Higher/Lower game with word usage
--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,10 @@
 export default function () {
     return (
         <>
-            <h1 className="text-6xl font-bold mt-24 text-center text-accent">My Telegram Statistics</h1>
+            <div className="flex items-end justify-center mt-24 gap-4">
+                <h1 className="text-6xl font-bold text-center text-accent">My Telegram Statistics</h1>
+                <img src="/logo.png" alt="logo" className="size-20"/>
+            </div>
             <p className="text-white text-lg mt-8 text-center mb-16">This project is open-source, ensuring transparency. No personal information or data is being saved. <br/><br/> Find more on my <a href="" className="italic underline">GitHub profile.</a></p>
         </>
     )
