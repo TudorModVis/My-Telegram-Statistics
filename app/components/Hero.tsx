84858fbaf27ba06bd33d080199273e0612e5dd91
Cebotarenco Tudor <tudorcebotarenco@gmail.com>
2024-07-10 19:44:17 +0300

    title update 2

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,10 +1,7 @@
 export default function () {
     return (
         <>
-            <div className="flex items-end justify-center mt-24 gap-4">
-                <h1 className="text-6xl font-bold text-center text-accent">My Telegram Statistics</h1>
-                <img src="/logo.png" alt="logo" className="size-20"/>
-            </div>
+            <h1 className="text-6xl font-bold mt-24 text-center text-accent">My Telegram Statistics</h1>
             <p className="text-white text-lg mt-8 text-center mb-16">This project is open-source, ensuring transparency. No personal information or data is being saved. <br/><br/> Find more on my <a href="" className="italic underline">GitHub profile.</a></p>
         </>
     )
